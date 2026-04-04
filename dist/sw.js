d3a649603081503c620e6924cdc6c7ae59710d1a
redgriffinanimatestudio <redgriffinanimatestudio@gmail.com>
2026-04-04 14:38:58 +0700

    Neural Industrialization (v2.32): 2026-04-04 14:38:58

--- dist/sw.js
+++ dist/sw.js
@@ -1,21 +1,9 @@
-const CACHE_NAME = 'rg-academy-v1';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("install", (e) => { self.skipWaiting(); });
+self.addEventListener("activate", (e) => {
+  caches.keys().then(names => { for (let name of names) caches.delete(name); });
+  self.registration.unregister().then(() => {
+    return self.clients.matchAll();
+  }).then(clients => {
+    clients.forEach(client => client.navigate(client.url));
+  });
 });
